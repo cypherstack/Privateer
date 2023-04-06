cypherstack/stack_wallet

add copy to clipboard functionality

lib/pages_desktop_specific/my_stack_view/wallet_view/sub_widgets/desktop_show_xpub_dialog.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:stackwallet/notifications/show_flush_bar.dart';
+import 'package:stackwallet/utilities/assets.dart';
+import 'package:stackwallet/utilities/clipboard_interface.dart';
 import 'package:stackwallet/utilities/text_styles.dart';
 import 'package:stackwallet/utilities/theme/stack_colors.dart';
 import 'package:stackwallet/widgets/desktop/desktop_dialog.dart';
@@ -11,9 +17,12 @@ class DesktopShowXpubDialog extends ConsumerStatefulWidget {
   const DesktopShowXpubDialog({
     Key? key,
     required this.xpub,
+    this.clipboardInterface = const ClipboardWrapper(),
   }) : super(key: key);
 
   final String xpub;
+
+  final ClipboardInterface clipboardInterface;
 
   static const String routeName = "/desktopShowXpubDialog";
 
@@ -23,14 +32,27 @@ class DesktopShowXpubDialog extends ConsumerStatefulWidget {
 }
 
 class _DesktopShowXpubDialog extends ConsumerState<DesktopShowXpubDialog> {
+  late ClipboardInterface _clipboardInterface;
+
   @override
   void initState() {
+    _clipboardInterface = widget.clipboardInterface;
     super.initState();
   }
 
   @override
   void dispose() {
     super.dispose();
+  }
+
+  Future<void> _copy() async {
+    await _clipboardInterface.setData(ClipboardData(text: widget.xpub));
+    unawaited(showFloatingFlushBar(
+      type: FlushBarType.info,
+      message: "Copied to clipboard",
+      iconAsset: Assets.svg.copy,
+      context: context,
+    ));
   }
 
   @override
@@ -71,6 +93,14 @@ class _DesktopShowXpubDialog extends ConsumerState<DesktopShowXpubDialog> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    PrimaryButton(
+                        width: 250,
+                        buttonHeight: ButtonHeight.xl,
+                        label: "Copy to clipboard",
+                        onPressed: () async {
+                          await _copy();
+                        }),
+                    const SizedBox(width: 16),
                     PrimaryButton(
                         width: 250,
                         buttonHeight: ButtonHeight.xl,

cypherstack/Privateer

lib/pages_desktop_specific/my_stack_view/wallet_view/sub_widgets/desktop_show_xpub_dialog.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:stackduo/utilities/text_styles.dart';
@@ -6,14 +9,20 @@ import 'package:stackduo/utilities/theme/stack_colors.dart';
 import 'package:stackduo/widgets/desktop/desktop_dialog.dart';
 import 'package:stackduo/widgets/desktop/desktop_dialog_close_button.dart';
 import 'package:stackduo/widgets/desktop/primary_button.dart';
+import 'package:stackduo/notifications/show_flush_bar.dart';
+import 'package:stackduo/utilities/assets.dart';
+import 'package:stackduo/utilities/clipboard_interface.dart';
 
 class DesktopShowXpubDialog extends ConsumerStatefulWidget {
   const DesktopShowXpubDialog({
     Key? key,
     required this.xpub,
+    this.clipboardInterface = const ClipboardWrapper(),
   }) : super(key: key);
 
   final String xpub;
+
+  final ClipboardInterface clipboardInterface;
 
   static const String routeName = "/desktopShowXpubDialog";
 
@@ -23,14 +32,27 @@ class DesktopShowXpubDialog extends ConsumerStatefulWidget {
 }
 
 class _DesktopShowXpubDialog extends ConsumerState<DesktopShowXpubDialog> {
+  late ClipboardInterface _clipboardInterface;
+
   @override
   void initState() {
+    _clipboardInterface = widget.clipboardInterface;
     super.initState();
   }
 
   @override
   void dispose() {
     super.dispose();
+  }
+
+  Future<void> _copy() async {
+    await _clipboardInterface.setData(ClipboardData(text: widget.xpub));
+    unawaited(showFloatingFlushBar(
+      type: FlushBarType.info,
+      message: "Copied to clipboard",
+      iconAsset: Assets.svg.copy,
+      context: context,
+    ));
   }
 
   @override
@@ -71,6 +93,14 @@ class _DesktopShowXpubDialog extends ConsumerState<DesktopShowXpubDialog> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    PrimaryButton(
+                        width: 250,
+                        buttonHeight: ButtonHeight.xl,
+                        label: "Copy to clipboard",
+                        onPressed: () async {
+                          await _copy();
+                        }),
+                    const SizedBox(width: 16),
                     PrimaryButton(
                         width: 250,
                         buttonHeight: ButtonHeight.xl,