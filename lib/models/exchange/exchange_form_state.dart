cypherstack/stack_wallet

initial Trocador integration

lib/models/exchange/exchange_form_state.dart
@@ -5,6 +5,7 @@ import 'package:stackwallet/models/exchange/response_objects/estimate.dart';
 import 'package:stackwallet/services/exchange/change_now/change_now_exchange.dart';
 import 'package:stackwallet/services/exchange/exchange.dart';
 import 'package:stackwallet/services/exchange/majestic_bank/majestic_bank_exchange.dart';
+import 'package:stackwallet/services/exchange/trocador/trocador_exchange.dart';
 import 'package:stackwallet/utilities/enums/exchange_rate_type_enum.dart';
 import 'package:stackwallet/utilities/logger.dart';
 
@@ -337,6 +338,16 @@ class ExchangeFormState extends ChangeNotifier {
           }
           break;
         case MajesticBankExchange.exchangeName:
+          if (!_exchangeSupported(
+            exchangeName: exchange.name,
+            sendCurrency: sendCurrency,
+            receiveCurrency: receiveCurrency,
+            exchangeRateType: exchangeRateType,
+          )) {
+            _exchange = ChangeNowExchange.instance;
+          }
+          break;
+        case TrocadorExchange.exchangeName:
           if (!_exchangeSupported(
             exchangeName: exchange.name,
             sendCurrency: sendCurrency,

lib/services/exchange/exchange.dart
@@ -8,6 +8,7 @@ import 'package:stackwallet/services/exchange/change_now/change_now_exchange.dar
 import 'package:stackwallet/services/exchange/exchange_response.dart';
 import 'package:stackwallet/services/exchange/majestic_bank/majestic_bank_exchange.dart';
 import 'package:stackwallet/services/exchange/simpleswap/simpleswap_exchange.dart';
+import 'package:stackwallet/services/exchange/trocador/trocador_exchange.dart';
 
 abstract class Exchange {
   static Exchange get defaultExchange => ChangeNowExchange.instance;
@@ -20,6 +21,8 @@ abstract class Exchange {
         return SimpleSwapExchange.instance;
       case MajesticBankExchange.exchangeName:
         return MajesticBankExchange.instance;
+      case TrocadorExchange.exchangeName:
+        return TrocadorExchange.instance;
       default:
         throw ArgumentError("Unknown exchange name");
     }

cypherstack/Privateer

(cherry picked from commit 05c4d01ee4b2bd43eb6dfff9ebe9b24535775ad3)

lib/models/exchange/exchange_form_state.dart
@@ -1,12 +1,12 @@
 import 'package:decimal/decimal.dart';
 import 'package:flutter/foundation.dart';
-import 'package:stackduo/models/exchange/aggregate_currency.dart';
-import 'package:stackduo/models/exchange/response_objects/estimate.dart';
-import 'package:stackduo/services/exchange/change_now/change_now_exchange.dart';
-import 'package:stackduo/services/exchange/exchange.dart';
-import 'package:stackduo/services/exchange/majestic_bank/majestic_bank_exchange.dart';
-import 'package:stackduo/utilities/enums/exchange_rate_type_enum.dart';
-import 'package:stackduo/utilities/logger.dart';
+import 'package:stackwallet/models/exchange/aggregate_currency.dart';
+import 'package:stackwallet/models/exchange/response_objects/estimate.dart';
+import 'package:stackwallet/services/exchange/change_now/change_now_exchange.dart';
+import 'package:stackwallet/services/exchange/exchange.dart';
+import 'package:stackwallet/services/exchange/majestic_bank/majestic_bank_exchange.dart';
+import 'package:stackwallet/utilities/enums/exchange_rate_type_enum.dart';
+import 'package:stackwallet/utilities/logger.dart';
 
 class ExchangeFormState extends ChangeNotifier {
   Exchange? _exchange;
@@ -337,6 +337,16 @@ class ExchangeFormState extends ChangeNotifier {
           }
           break;
         case MajesticBankExchange.exchangeName:
+          if (!_exchangeSupported(
+            exchangeName: exchange.name,
+            sendCurrency: sendCurrency,
+            receiveCurrency: receiveCurrency,
+            exchangeRateType: exchangeRateType,
+          )) {
+            _exchange = ChangeNowExchange.instance;
+          }
+          break;
+        case TrocadorExchange.exchangeName:
           if (!_exchangeSupported(
             exchangeName: exchange.name,
             sendCurrency: sendCurrency,

lib/services/exchange/exchange.dart
@@ -1,13 +1,13 @@
 import 'package:decimal/decimal.dart';
-import 'package:stackduo/models/exchange/response_objects/estimate.dart';
-import 'package:stackduo/models/exchange/response_objects/range.dart';
-import 'package:stackduo/models/exchange/response_objects/trade.dart';
-import 'package:stackduo/models/isar/exchange_cache/currency.dart';
-import 'package:stackduo/models/isar/exchange_cache/pair.dart';
-import 'package:stackduo/services/exchange/change_now/change_now_exchange.dart';
-import 'package:stackduo/services/exchange/exchange_response.dart';
-import 'package:stackduo/services/exchange/majestic_bank/majestic_bank_exchange.dart';
-import 'package:stackduo/services/exchange/simpleswap/simpleswap_exchange.dart';
+import 'package:stackwallet/models/exchange/response_objects/estimate.dart';
+import 'package:stackwallet/models/exchange/response_objects/range.dart';
+import 'package:stackwallet/models/exchange/response_objects/trade.dart';
+import 'package:stackwallet/models/isar/exchange_cache/currency.dart';
+import 'package:stackwallet/models/isar/exchange_cache/pair.dart';
+import 'package:stackwallet/services/exchange/change_now/change_now_exchange.dart';
+import 'package:stackwallet/services/exchange/exchange_response.dart';
+import 'package:stackwallet/services/exchange/majestic_bank/majestic_bank_exchange.dart';
+import 'package:stackwallet/services/exchange/simpleswap/simpleswap_exchange.dart';
 
 abstract class Exchange {
   static Exchange get defaultExchange => ChangeNowExchange.instance;
@@ -20,6 +20,8 @@ abstract class Exchange {
         return SimpleSwapExchange.instance;
       case MajesticBankExchange.exchangeName:
         return MajesticBankExchange.instance;
+      case TrocadorExchange.exchangeName:
+        return TrocadorExchange.instance;
       default:
         throw ArgumentError("Unknown exchange name");
     }