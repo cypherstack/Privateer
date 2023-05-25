cypherstack/stack_wallet

v2 theme assets

lib/themes/coin_icon_provider.dart
@@ -1,38 +1,44 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stackwallet/models/isar/stack_theme.dart';
 import 'package:stackwallet/themes/theme_providers.dart';
 import 'package:stackwallet/utilities/enums/coin_enum.dart';
 
 final coinIconProvider = Provider.family<String, Coin>((ref, coin) {
-  final assets = ref.watch(themeProvider).assets;
-  switch (coin) {
-    case Coin.bitcoin:
-    case Coin.bitcoinTestNet:
-      return assets.bitcoin;
-    case Coin.litecoin:
-    case Coin.litecoinTestNet:
-      return assets.litecoin;
-    case Coin.bitcoincash:
-    case Coin.bitcoincashTestnet:
-      return assets.bitcoincash;
-    case Coin.dogecoin:
-    case Coin.dogecoinTestNet:
-      return assets.dogecoin;
-    case Coin.eCash:
-      return assets.bitcoin;
-    case Coin.epicCash:
-      return assets.epicCash;
-    case Coin.firo:
-    case Coin.firoTestNet:
-      return assets.firo;
-    case Coin.monero:
-      return assets.monero;
-    case Coin.wownero:
-      return assets.wownero;
-    case Coin.namecoin:
-      return assets.namecoin;
-    case Coin.particl:
-      return assets.particl;
-    case Coin.ethereum:
-      return assets.ethereum;
+  final assets = ref.watch(themeAssetsProvider);
+
+  if (assets is ThemeAssets) {
+    switch (coin) {
+      case Coin.bitcoin:
+      case Coin.bitcoinTestNet:
+        return assets.bitcoin;
+      case Coin.litecoin:
+      case Coin.litecoinTestNet:
+        return assets.litecoin;
+      case Coin.bitcoincash:
+      case Coin.bitcoincashTestnet:
+        return assets.bitcoincash;
+      case Coin.dogecoin:
+      case Coin.dogecoinTestNet:
+        return assets.dogecoin;
+      case Coin.eCash:
+        return assets.bitcoin;
+      case Coin.epicCash:
+        return assets.epicCash;
+      case Coin.firo:
+      case Coin.firoTestNet:
+        return assets.firo;
+      case Coin.monero:
+        return assets.monero;
+      case Coin.wownero:
+        return assets.wownero;
+      case Coin.namecoin:
+        return assets.namecoin;
+      case Coin.particl:
+        return assets.particl;
+      case Coin.ethereum:
+        return assets.ethereum;
+    }
+  } else {
+    return (assets as ThemeAssetsV2).coinIcons[coin]!;
   }
 });

lib/themes/coin_image_provider.dart
@@ -1,81 +1,92 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stackwallet/models/isar/stack_theme.dart';
 import 'package:stackwallet/themes/theme_providers.dart';
 import 'package:stackwallet/utilities/enums/coin_enum.dart';
 
 final coinImageProvider = Provider.family<String, Coin>((ref, coin) {
-  final assets = ref.watch(themeProvider).assets;
-  switch (coin) {
-    case Coin.bitcoin:
-      return assets.bitcoinImage;
-    case Coin.litecoin:
-    case Coin.litecoinTestNet:
-      return assets.litecoinImage;
-    case Coin.bitcoincash:
-      return assets.bitcoincashImage;
-    case Coin.dogecoin:
-      return assets.dogecoinImage;
-    case Coin.eCash:
-      return assets.bitcoinImage;
-    case Coin.epicCash:
-      return assets.epicCashImage;
-    case Coin.firo:
-      return assets.firoImage;
-    case Coin.monero:
-      return assets.moneroImage;
-    case Coin.wownero:
-      return assets.wowneroImage;
-    case Coin.namecoin:
-      return assets.namecoinImage;
-    case Coin.particl:
-      return assets.particlImage;
-    case Coin.bitcoinTestNet:
-      return assets.bitcoinImage;
-    case Coin.bitcoincashTestnet:
-      return assets.bitcoincashImage;
-    case Coin.firoTestNet:
-      return assets.firoImage;
-    case Coin.dogecoinTestNet:
-      return assets.dogecoinImage;
-    case Coin.ethereum:
-      return assets.ethereumImage;
+  final assets = ref.watch(themeAssetsProvider);
+
+  if (assets is ThemeAssets) {
+    switch (coin) {
+      case Coin.bitcoin:
+        return assets.bitcoinImage;
+      case Coin.litecoin:
+      case Coin.litecoinTestNet:
+        return assets.litecoinImage;
+      case Coin.bitcoincash:
+        return assets.bitcoincashImage;
+      case Coin.dogecoin:
+        return assets.dogecoinImage;
+      case Coin.eCash:
+        return assets.bitcoinImage;
+      case Coin.epicCash:
+        return assets.epicCashImage;
+      case Coin.firo:
+        return assets.firoImage;
+      case Coin.monero:
+        return assets.moneroImage;
+      case Coin.wownero:
+        return assets.wowneroImage;
+      case Coin.namecoin:
+        return assets.namecoinImage;
+      case Coin.particl:
+        return assets.particlImage;
+      case Coin.bitcoinTestNet:
+        return assets.bitcoinImage;
+      case Coin.bitcoincashTestnet:
+        return assets.bitcoincashImage;
+      case Coin.firoTestNet:
+        return assets.firoImage;
+      case Coin.dogecoinTestNet:
+        return assets.dogecoinImage;
+      case Coin.ethereum:
+        return assets.ethereumImage;
+    }
+  } else {
+    return (assets as ThemeAssetsV2).coinImages[coin]!;
   }
 });
 
 final coinImageSecondaryProvider = Provider.family<String, Coin>((ref, coin) {
-  final assets = ref.watch(themeProvider).assets;
-  switch (coin) {
-    case Coin.bitcoin:
-      return assets.bitcoinImageSecondary;
-    case Coin.litecoin:
-    case Coin.litecoinTestNet:
-      return assets.litecoinImageSecondary;
-    case Coin.bitcoincash:
-      return assets.bitcoincashImageSecondary;
-    case Coin.dogecoin:
-      return assets.dogecoinImageSecondary;
-    case Coin.eCash:
-      return assets.bitcoinImageSecondary;
-    case Coin.epicCash:
-      return assets.epicCashImageSecondary;
-    case Coin.firo:
-      return assets.firoImageSecondary;
-    case Coin.monero:
-      return assets.moneroImageSecondary;
-    case Coin.wownero:
-      return assets.wowneroImageSecondary;
-    case Coin.namecoin:
-      return assets.namecoinImageSecondary;
-    case Coin.particl:
-      return assets.particlImageSecondary;
-    case Coin.bitcoinTestNet:
-      return assets.bitcoinImageSecondary;
-    case Coin.bitcoincashTestnet:
-      return assets.bitcoincashImageSecondary;
-    case Coin.firoTestNet:
-      return assets.firoImageSecondary;
-    case Coin.dogecoinTestNet:
-      return assets.dogecoinImageSecondary;
-    case Coin.ethereum:
-      return assets.ethereumImageSecondary;
+  final assets = ref.watch(themeAssetsProvider);
+
+  if (assets is ThemeAssets) {
+    switch (coin) {
+      case Coin.bitcoin:
+        return assets.bitcoinImageSecondary;
+      case Coin.litecoin:
+      case Coin.litecoinTestNet:
+        return assets.litecoinImageSecondary;
+      case Coin.bitcoincash:
+        return assets.bitcoincashImageSecondary;
+      case Coin.dogecoin:
+        return assets.dogecoinImageSecondary;
+      case Coin.eCash:
+        return assets.bitcoinImageSecondary;
+      case Coin.epicCash:
+        return assets.epicCashImageSecondary;
+      case Coin.firo:
+        return assets.firoImageSecondary;
+      case Coin.monero:
+        return assets.moneroImageSecondary;
+      case Coin.wownero:
+        return assets.wowneroImageSecondary;
+      case Coin.namecoin:
+        return assets.namecoinImageSecondary;
+      case Coin.particl:
+        return assets.particlImageSecondary;
+      case Coin.bitcoinTestNet:
+        return assets.bitcoinImageSecondary;
+      case Coin.bitcoincashTestnet:
+        return assets.bitcoincashImageSecondary;
+      case Coin.firoTestNet:
+        return assets.firoImageSecondary;
+      case Coin.dogecoinTestNet:
+        return assets.dogecoinImageSecondary;
+      case Coin.ethereum:
+        return assets.ethereumImageSecondary;
+    }
+  } else {
+    return (assets as ThemeAssetsV2).coinSecondaryImages[coin]!;
   }
 });

cypherstack/Privateer

lib/themes/coin_icon_provider.dart
@@ -1,15 +1,20 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stackduo/models/isar/stack_theme.dart';
 import 'package:stackduo/themes/theme_providers.dart';
 import 'package:stackduo/utilities/enums/coin_enum.dart';
 
 final coinIconProvider = Provider.family<String, Coin>((ref, coin) {
-  final assets = ref.watch(themeProvider).assets;
-  switch (coin) {
-    case Coin.bitcoin:
-    case Coin.bitcoinTestNet:
-      return assets.bitcoin;
+  final assets = ref.watch(themeAssetsProvider);
 
-    case Coin.monero:
-      return assets.monero;
+  if (assets is ThemeAssets) {
+    switch (coin) {
+      case Coin.bitcoin:
+      case Coin.bitcoinTestNet:
+        return assets.bitcoin;
+      case Coin.monero:
+        return assets.monero;
+    }
+  } else {
+    return (assets as ThemeAssetsV2).coinIcons[coin]!;
   }
 });

lib/themes/coin_image_provider.dart
@@ -1,27 +1,38 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stackduo/models/isar/stack_theme.dart';
 import 'package:stackduo/themes/theme_providers.dart';
 import 'package:stackduo/utilities/enums/coin_enum.dart';
 
 final coinImageProvider = Provider.family<String, Coin>((ref, coin) {
-  final assets = ref.watch(themeProvider).assets;
-  switch (coin) {
-    case Coin.bitcoin:
-    case Coin.bitcoinTestNet:
-      return assets.bitcoinImage;
+  final assets = ref.watch(themeAssetsProvider);
 
-    case Coin.monero:
-      return assets.moneroImage;
+  if (assets is ThemeAssets) {
+    switch (coin) {
+      case Coin.bitcoin:
+      case Coin.bitcoinTestNet:
+        return assets.bitcoinImage;
+
+      case Coin.monero:
+        return assets.moneroImage;
+    }
+  } else {
+    return (assets as ThemeAssetsV2).coinImages[coin]!;
   }
 });
 
 final coinImageSecondaryProvider = Provider.family<String, Coin>((ref, coin) {
-  final assets = ref.watch(themeProvider).assets;
-  switch (coin) {
-    case Coin.bitcoin:
-    case Coin.bitcoinTestNet:
-      return assets.bitcoinImageSecondary;
+  final assets = ref.watch(themeAssetsProvider);
 
-    case Coin.monero:
-      return assets.moneroImageSecondary;
+  if (assets is ThemeAssets) {
+    switch (coin) {
+      case Coin.bitcoin:
+      case Coin.bitcoinTestNet:
+        return assets.bitcoinImageSecondary;
+
+      case Coin.monero:
+        return assets.moneroImageSecondary;
+    }
+  } else {
+    return (assets as ThemeAssetsV2).coinSecondaryImages[coin]!;
   }
 });